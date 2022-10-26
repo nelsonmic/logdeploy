26b941cea7f47a0b31faea9fe5fd962f2fa3d8de
nelsonmichael <iamnelsonmichael@gmail.com>
2022-10-26 16:00:25 +0100

    set up routing

--- src/index.js
+++ src/index.js
@@ -2,12 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import About from "./routes/About";
+import Careers from "./routes/Careers";
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+    {
+    path: "/about",
+    element: <About/>
+  },
+    {
+    path: "/careers",
+    element: <Careers />
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
